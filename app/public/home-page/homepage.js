48b130fcd6a485e060ae00cbb3882b640b59c5ab
jkr59 <jkr59@drexel.edu>
2024-08-17 21:57:13 -0400

    Added logout functionality to homepage

--- app/public/home-page/homepage.js
+++ app/public/home-page/homepage.js
@@ -1,21 +1,23 @@
 import { auth } from '../firebaseConfig.js';
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-
 const updateUI = (user) => {
     const loginButton = document.getElementById('login-button');
-    const logoutButton = document.getElementById('logout-button');
     const userEmailDisplay = document.getElementById('user-email');
+    const dropdown = document.getElementById('dropdown');
 
     if (user) {
         // User is signed in
         loginButton.style.display = 'none';
         userEmailDisplay.textContent = `${user.email}`;
         userEmailDisplay.style.display = 'block';
+        userEmailDisplay.addEventListener('click', () => {
+            userEmailDisplay.classList.toggle('active'); // Toggle dropdown visibility
+        });
     } else {
-        // User is signed out 
-        userEmailDisplay.style.display = 'none'; 
-        logoutButton.style.display = 'none'; 
+        // User is signed out
+        userEmailDisplay.style.display = 'none';
+        dropdown.style.display = 'none';
     }
 };
 
@@ -28,7 +30,7 @@ document.getElementById('logout-button').addEventListener('click', async () => {
         await signOut(auth);
         localStorage.removeItem('authToken');
         localStorage.removeItem('userId');
-        window.location.href = "/login-page";
+        window.location.href = "/";
     } catch (error) {
         console.error('Error signing out:', error);
         alert('Error signing out. Please try again.');
